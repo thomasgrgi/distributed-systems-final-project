fc79a05bcccb4ae529051ea320399c938b953a31
n23bodi2 <noe.bodineau@imt-atlantique.net>
2025-12-09 13:42:45 +0700

    dockerisation

--- ticket_queue/utils/redis_client.py
+++ ticket_queue/utils/redis_client.py
@@ -2,7 +2,7 @@ import redis
 
 def get_redis_connection():
     return redis.Redis(
-        host="localhost",
+        host="redis",
         port=6379,
         db=0,
         decode_responses=True
